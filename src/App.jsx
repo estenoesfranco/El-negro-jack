ae77f11212a9e4d888a7cf3805308b51f94891f2
LoreGal24 <163057427+LoreGal24@users.noreply.github.com>
2024-08-27 10:09:47 -0300

    Inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+
+function App() {
+
+
+    
+  
+
+  return (
+    <div> 
+
+      </div>
+     
+
+
+  );
+ }
+
+export default App;
